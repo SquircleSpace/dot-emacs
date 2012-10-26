2a462d78a643cce759e19fbf87bfb268cb131dd0
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2012-10-26 01:02:21 -0700

    Organized init.el a bit better.

--- init.el
+++ init.el
@@ -1,27 +1,7 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global
 
-(defun pull-config ()
-  "Fetch configuration from a server"
-  (interactive)
-  (with-temp-buffer
-    (insert-file-contents "/ssh:bradjensen@bradjensen.net:~/.emacs")
-    (write-region nil nil user-init-file)))
-
-(defun push-config ()
-  "Push configuration to a server"
-  (interactive)
-  (with-temp-buffer
-    (insert-file-contents user-init-file)
-    (write-region nil nil "/ssh:bradjensen@bradjensen.net:~/.emacs")))
-
 (defun set-global-options ()
-
-  ;; Set whitespace settings
-  (require 'whitespace)
-  (setq whitespace-style
-	'(trailing empty lines-tail indentation face))
-  (setq whitespace-action '(auto-cleanup))
 
   ;; Show Column numbers
   (setq column-number-mode t)
@@ -60,10 +40,21 @@
   (global-set-key [(mouse-5)] '(lambda () (interactive) (smooth-scroll 3 1)))
   (global-set-key [(mouse-4)] '(lambda () (interactive) (smooth-scroll 3 -1)))
 
-  ;; Use aspell for spellchecking
+  ;; Use hunspell for spellchecking
   (setq-default ispell-program-name "hunspell"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Whitespace
+
+(defun set-whitespace-options ()
+  ;; Set whitespace settings
+  (require 'whitespace)
+  (setq whitespace-style
+	'(trailing empty lines-tail indentation face))
+  (setq whitespace-action '(auto-cleanup))
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Org mode
 
 (defun set-org-mode-options ()
@@ -72,74 +63,74 @@
   (add-hook 'org-mode-hook 'org-indent-mode)
   (add-hook 'org-mode-hook 'flyspell-mode)
 
-  ;; Use org-mode 7.7
+  ;; Use private copy of org-mode
   (add-to-list 'load-path
                (expand-file-name "~/.emacs.d/elisp/org-mode/lisp"))
   (require 'org)
   (require 'org-latex)
   (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
-  ;; Better subsections for pdf export org-mode
-  (require 'org-latex)
-  (unless (boundp 'org-export-latex-classes)
-    (setq org-export-latex-classes nil))
-  (add-to-list 'org-export-latex-classes
-	       '("article"
-		 "\\documentclass{article}"
-		 ("\\section{%s}" . "\\section*{%s}")
-		 ("\\subsection{%s}" . "\\subsection*{%s}")
-		 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-		 ("\\paragraph{%s}" . "\\paragraph*{%s}")
-		 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
-  ;; Used Minted for better code export
-  (setq org-export-latex-listings 'minted)
-  ;;(setq org-export-latex-custom-lang-environments
-  ;;      '(
-  ;;        (emacs-lisp "common-lispcode")
-  ;;        ))
-  (setq org-export-latex-minted-options
-	'(("frame" "lines")
-	  ("fontsize" "\\scriptsize")
-	  ("linenos" "")))
-  (setq org-latex-to-pdf-process
-	'("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-	  "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-	  "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
-
-  ;; Org code languages
-  ;(org-babel-do-load-languages
-  ; 'org-babel-load-languages
-  ; '((emacs-lisp . t)
-  ;   (R . t)
-  ;   (python . t)))
+  ;;;; Better subsections for pdf export org-mode
+  ;;(require 'org-latex)
+  ;;(unless (boundp 'org-export-latex-classes)
+  ;;  (setq org-export-latex-classes nil))
+  ;;(add-to-list 'org-export-latex-classes
+  ;;             '("article"
+  ;;      	 "\\documentclass{article}"
+  ;;      	 ("\\section{%s}" . "\\section*{%s}")
+  ;;      	 ("\\subsection{%s}" . "\\subsection*{%s}")
+  ;;      	 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+  ;;      	 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+  ;;      	 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  ;;
+  ;;;; Used Minted for better code export
+  ;;(setq org-export-latex-listings 'minted)
+  ;;;;(setq org-export-latex-custom-lang-environments
+  ;;;;      '(
+  ;;;;        (emacs-lisp "common-lispcode")
+  ;;;;        ))
+  ;;(setq org-export-latex-minted-options
+  ;;      '(("frame" "lines")
+  ;;        ("fontsize" "\\scriptsize")
+  ;;        ("linenos" "")))
+  ;;(setq org-latex-to-pdf-process
+  ;;      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+  ;;        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+  ;;        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+  ;;
+  ;;;; Org code languages
+  ;;;(org-babel-do-load-languages
+  ;;; 'org-babel-load-languages
+  ;;; '((emacs-lisp . t)
+  ;;;   (R . t)
+  ;;;   (python . t)))
   )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Python
 
 (defun set-python-options ()
 
   ;; Make running python mode work with packages
   (defun python-reinstate-current-directory ()
-    "When running Python, add the current directory ('') to the head of sys.path.
-For reasons unexplained, run-python passes arguments to the
-interpreter that explicitly remove '' from sys.path. This means
-that, for example, using `python-send-buffer' in a buffer
-visiting a module's code will fail to find other modules in the
-same directory.
+    "When running Python, add the current directory ('') to the head
+of sys.path.  For reasons unexplained, run-python passes arguments to
+the interpreter that explicitly remove '' from sys.path. This means
+that, for example, using `python-send-buffer' in a buffer visiting a
+module's code will fail to find other modules in the same directory.
 
-Adding this function to `inferior-python-mode-hook' reinstates
-the current directory in Python's search path."
+Adding this function to `inferior-python-mode-hook' reinstates the
+current directory in Python's search path."
     (python-send-string "sys.path[0:0] = ['']"))
   
   (add-hook 'inferior-python-mode-hook
 	    'python-reinstate-current-directory)
   
   (add-hook 'python-mode-hook
-	    'whitespace-mode))
+	    'whitespace-mode)
+  )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Haskell
 
 (defun set-haskell-options ()
@@ -150,22 +141,20 @@ the current directory in Python's search path."
   (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
   )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Misc
 
-(defun set-other-options ()
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MATLAB
 
-  ;; Make shell mode use zsh
-  (setq explicit-shell-file-name "/bin/zsh")
-
+(defun set-matlab-options ()  
   ;; Add matlab support
   (add-to-list 'load-path "~/.emacs.d/elisp/mlab")
   (load-library "matlab-load")
+  )
 
-  ;; Add CS70 Rubric support
-  (load "~/.emacs.d/elisp/rubric.el")
-  (add-to-list 'auto-mode-alist '("Rubric\.\*\\.txt" . rubric-mode))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C++
 
+(defun set-c++-options ()
   ;; Set C++ style
   (setq c-default-style
 	(quote
@@ -176,18 +165,46 @@ the current directory in Python's search path."
 	  (other . "gnu"))))
 
   (add-hook 'c-mode-common-hook
-	    'whitespace-mode))
+	    'whitespace-mode)
+  )
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CS 70
+
+(defun set-cs70-options ()
+  ;; Add CS70 Rubric support
+  (load "~/.emacs.d/elisp/rubric.el")
+  (add-to-list 'auto-mode-alist '("Rubric\.\*\\.txt" . rubric-mode))
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Misc
+
+(defun set-other-options ()
+
+  ;; Make shell mode use zsh
+  (setq explicit-shell-file-name "/bin/zsh")
+
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Apply options
 
 (defun set-all-options ()
-  
+
   (set-global-options)
+  (set-other-options)
+
+  ;; Mode options
+  (set-whitespace-options)
   (set-org-mode-options)
+  (set-cs70-options)
+
+  ;; Programming options
   (set-python-options)
   (set-haskell-options)
-  (set-other-options)
+  (set-matlab-options)
+  (set-c++-options)
   )
 
 (set-all-options)
