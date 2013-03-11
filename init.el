d73f7774040fb5057d5642e2c9d61986ab8dcce4
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-03-11 15:06:15 -0700

    Added mac-friendly keyboard shortcuts.

--- init.el
+++ init.el
@@ -226,8 +226,18 @@ current directory in Python's search path."
   ;; Detect system
   ; Mac
   (when (eq system-type 'darwin)
+    ;; keyboard stuff
     (setq mac-command-modifier (quote super))
     (setq mac-option-modifier (quote meta))
+    (global-set-key [(super a)] 'mark-whole-buffer)
+    (global-set-key [(super v)] 'yank)
+    (global-set-key [(super c)] 'kill-ring-save)
+    (global-set-key [(super x)] 'kill-region)
+    (global-set-key [(super s)] 'save-buffer)
+    (global-set-key [(super l)] 'goto-line)
+    (global-set-key [(super w)]
+                    (lambda () (interactive) (delete-window)))
+    (global-set-key [(super z)] 'undo)
     (setq TeX-view-program-list
           (quote (("Open" "open %s.pdf"))))
     (setq TeX-view-program-selection
