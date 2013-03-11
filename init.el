a8e8739a9b811adc0131dc8da2a81cbfa31c519d
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-03-10 20:35:10 -0700

    Whitespace mode is a bit less painful to look at.

--- init.el
+++ init.el
@@ -383,4 +383,11 @@ current directory in Python's search path."
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-empty ((t (:background "dark slate gray" :foreground "#dc322f"))))
+ '(whitespace-trailing ((t (:background "dark slate gray" :foreground "#dc322f" :inverse-video nil :underline nil :slant normal :weight bold)))))
 (set-all-options)
