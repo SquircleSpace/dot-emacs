85aeb986d87af3c5e18b14635143144c5e6ff4d5
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-05-07 01:10:23 -0700

    autocomplete-config now builds the clang-complete executable if it is needed.

--- config/autocomplete-config.el
+++ config/autocomplete-config.el
@@ -2,6 +2,7 @@
 
 (defun set-autocomplete-options ()
   (require-package 'auto-complete)
+  ; Unfortunately, this package doesn't include the binary we need.
   (require-package 'auto-complete-clang-async)
   (require-package 'yasnippet)
   (require 'auto-complete-config)
@@ -18,7 +19,16 @@
     (setq ac-sources '(ac-source-clang-async)) ; We only need clang-async
     (ac-clang-launch-completion-process)) ; Need to explicitly launch process
 
-  (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+  ;; Tell clang-async where to look for clang-complete
+  (setq ac-clang-complete-executable
+        "~/.emacs.d/config/autocomplete-config/clang-complete")
+  ;; Build it if it isn't there
+  (unless (file-exists-p ac-clang-complete-executable)
+    (call-process "make" nil nil nil "-C"
+                   "~/.emacs.d/config/autocomplete-config/")
+    (unless (file-exists-p ac-clang-complete-executable)
+      (error "Couldn't build clang-complete")))
+    
   (add-to-list 'ac-sources 'ac-source-clang-async)
   (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
   (add-hook 'objc-mode-hook 'ac-cc-mode-setup)
