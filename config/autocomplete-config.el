3d3745bd9ce4a237e6716afe1c61d7c47cda2ada
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-05-06 22:29:29 -0700

    Added support for using clang to autocomplete.

--- config/autocomplete-config.el
+++ config/autocomplete-config.el
@@ -2,9 +2,26 @@
 
 (defun set-autocomplete-options ()
   (require-package 'auto-complete)
+  (require-package 'auto-complete-clang-async)
+  (require-package 'yasnippet)
   (require 'auto-complete-config)
+  (require 'auto-complete-clang-async)
+  (require 'yasnippet)
+
+  ;; Initalize autocomplete
   (ac-config-default)
   (ac-flyspell-workaround)
+
+  ;; Setup clang-async
+  (defun ac-cc-mode-setup ()
+    (yas-minor-mode-on) ; Strange things happen if yas-minor-mode isn't on
+    (setq ac-sources '(ac-source-clang-async)) ; We only need clang-async
+    (ac-clang-launch-completion-process)) ; Need to explicitly launch process
+
+  (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+  (add-to-list 'ac-sources 'ac-source-clang-async)
+  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'objc-mode-hook 'ac-cc-mode-setup)
   )
 
 (set-autocomplete-options)
