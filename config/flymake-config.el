ea9beadbaa8f56fe2e9b1f442a70cae418e4818a
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-05-07 14:51:42 -0700

    flymake-mode enabled for c-like modes by default.

--- config/flymake-config.el
+++ config/flymake-config.el
@@ -6,6 +6,11 @@
   (push '("\\.\\(?:c\\(?:xx\\|pp\\|\\+\\+\\)?\\|CC\\|h\\|hpp\\|m\\)\\'"
           ac-clang-syntax-check)
         flymake-allowed-file-name-masks)
+
+  ;; Start up flymake mode when it makes sense
+  (let ((modes '(c-mode-common-hook)))
+    (dolist (mode modes)
+      (add-hook mode 'flymake-mode)))
   )
 
 (set-flymake-options)
