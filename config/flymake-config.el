296445b231ed3987f03f1cf10bbe329b437ce899
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-05-06 22:29:01 -0700

    Added support for using clang to flymake.

--- config/flymake-config.el
+++ config/flymake-config.el
@@ -1,7 +1,13 @@
 (require 'package-config)
 
 (defun set-flymake-options ()
-  (require-package 'flymake 'flymake-cursor)
+  (require-package 'flymake 'flymake-cursor 'auto-complete-clang-async)
+
+  (require 'auto-complete-clang-async)
+  (require 'flymake)
+  (push '("\\.\\(?:c\\(?:xx\\|pp\\|\\+\\+\\)?\\|CC\\|h\\|hpp\\|m\\)\\'"
+          ac-clang-syntax-check)
+        flymake-allowed-file-name-masks)
   )
 
 (set-flymake-options)
