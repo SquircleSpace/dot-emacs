2af718c7d537a38d429a8d4df6d9935ff4e7547a
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-05-08 00:25:41 -0700

    objc-config now just uses flymake-config and autocomplete-config.

--- config/objc-config.el
+++ config/objc-config.el
@@ -1,9 +1,6 @@
 (defun set-objc-options ()
-  (require 'auto-complete-config)
   (require 'autocomplete-config)
-
-  (add-hook 'objc-mode-hook 'flymake-mode)
-  (add-to-list 'ac-modes 'objc-mode)
+  (require 'flymake-config)
   )
 
 (set-objc-options)
