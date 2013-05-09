0939aabcc13673bee39751b8c3eb4d54bd7f5181
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-05-09 05:06:52 -0700

    Fixed a bug in fringe-config that caused scrolling to not work as expected.

--- config/fringe-config.el
+++ config/fringe-config.el
@@ -8,16 +8,28 @@
   (setq whitespace-style (delete 'empty whitespace-style))
 
   ;; By default, scrolling in fringes and margins is unbound. This is
-  ;; silly. Make it do the sane thing and scroll like normal.
-  (dolist (prefix '("" "double-" "triple-"))
-    (dolist (direction '("up" "down" "left" "right"))
-      (dolist (side '("left" "right"))
-        (dolist (type '("fringe" "margin"))
-          (let* ((fringe-str (concat "<" side "-" type ">"))
-                 (wheel-str (concat "<" prefix "wheel-" direction ">"))
-                 (from-kbd (kbd (concat fringe-str " " wheel-str)))
-                 (to-fn (global-key-binding (kbd wheel-str))))
-            (global-set-key from-kbd to-fn))))))
+  ;; silly. Make it do the sane thing and scroll like normal. Be
+  ;; careful, though! At least one Emacs mac port rebinds the scroll
+  ;; keys after init! So, we go through a layer of indirection and
+  ;; lookup the correct function to call every time the scroll event
+  ;; happens.
+  (dolist (direction '("up" "down" "left" "right"))
+    (dolist (side '("left" "right"))
+      (dolist (type '("fringe" "margin"))
+        (let* ((fringe-str (concat "<" side "-" type ">"))
+               (wheel-str (concat "<wheel-" direction ">"))
+               (from-kbd (kbd (concat fringe-str " " wheel-str)))
+               (to-fn `(lambda (event)
+                         ; Need to be interactive, for some
+                         ; reason. Copied paramater from function we
+                         ; are wraping.
+                         (interactive (list last-input-event))
+                         ; Call the real function, but bake the key to
+                         ; lookup into the lambda function
+                         (call-interactively (global-key-binding
+                                              ,(kbd wheel-str))
+                                             nil [event]))))
+          (global-set-key from-kbd (eval to-fn))))))
   )
 
 ;; Can't set fringe options without a window system
