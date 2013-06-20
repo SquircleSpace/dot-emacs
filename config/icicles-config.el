b6be8605154aa0b19d77e15929c973af7a210b08
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-06-19 22:48:53 -0700

    Stylistic changes.

--- config/icicles-config.el
+++ config/icicles-config.el
@@ -1,12 +1,10 @@
 (defun set-icicles-options ()
   (require-package 'icicles)
   (require-package 'fuzzy-match)
-  (require 'icicles)
 
   (setq icicle-TAB-completion-methods '(fuzzy vanilla basic))
 
-  (icy-mode)
-  )
+  (icy-mode))
 
 (set-icicles-options)
 
--- config/ido-config.el
+++ config/ido-config.el
@@ -4,22 +4,20 @@
   (require 'ido-ubiquitous)
   (require 'ido-yes-or-no)
 
-  ; Turn on ido
+  ;; Turn on ido
   (ido-mode)
 
-  ; Use flex matching
+  ;; Use flex matching
   (setq ido-enable-flex-matching t)
 
-  ; More ido is more better
+  ;; More ido is more better
   (ido-everywhere)
   (ido-ubiquitous-mode)
   (ido-yes-or-no-mode)
 
-  ; Make ido not do that annoying "search for recent files matching
-  ; what you typed" thing.
-  (setq ido-auto-merge-work-directories-length -1)
-
-  )
+  ;; Make ido not do that annoying "search for recent files matching
+  ;; what you typed" thing.
+  (setq ido-auto-merge-work-directories-length -1))
 
 (set-ido-options)
 
