4067c5daef989ec5841efd3236c77f8b18798f62
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-06-19 22:48:13 -0700

    Cleaned up Haskell mode code a bit. Added flymake support.

--- config/haskell-config.el
+++ config/haskell-config.el
@@ -1,20 +1,21 @@
 (defun set-haskell-options ()
   (require-package 'haskell-mode)
+  (require-package 'flymake-haskell-multi)
 
-  (eval-after-load 'auto-complete
-    '(progn
-       (add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/"))
-       (require 'haskell-ac)
-       (require 'auto-complete-config)
-       (add-to-list 'ac-modes 'haskell-mode)
-       )
-    )
+  ;; Set up autocomplete
+  (require 'autocomplete-config)
+  (add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp/"))
+  (require 'haskell-ac)
+  (add-to-list 'ac-modes 'haskell-mode)
+
   (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
   (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
   (add-hook 'haskell-mode-hook 'font-lock-mode)
   (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
 
-  )
+  ;; Set up flymake
+  (require 'flymake-config)
+  (add-hook 'haskell-mode-hook 'flymake-haskell-multi-load))
 
 (set-haskell-options)
 
