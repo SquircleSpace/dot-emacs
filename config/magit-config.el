067f9f32c81fdf749c0bfcf290195bebe70569d5
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-07-16 20:31:22 -0700

    Added magit-inotify.

--- config/magit-config.el
+++ config/magit-config.el
@@ -3,6 +3,12 @@
   (require-package 'magit)
   (global-set-key (kbd "C-c s") 'magit-status)
 
+  ;; If Emacs supports it, install magit-inotify
+  (when ((and (> emacs-major-version 24)
+              (> emacs-minor-version 4)))
+    (require-package 'magit-inotify)
+    (add-hook 'magit-mode-hook 'magit-inotify-mode))
+
   )
 
 (set-magit-options)
