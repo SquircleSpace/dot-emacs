011ae4f228909c6c314277bb08d4cf087b9fd06c
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2013-10-11 10:56:06 -0700

    Added smooth scrolling for some GUI versions.

--- config/mouse-config.el
+++ config/mouse-config.el
@@ -1,26 +1,40 @@
-(defun set-mouse-options ()
-  ; gui emacs already has mouse support
-  (unless window-system
-    ;; Enable mouse
-    (require 'mouse)
-    (xterm-mouse-mode t)
-    (defun track-mouse (e))
-    (setq mouse-sel-mode t)
+(defun smooth-scroll (number-lines increment)
+  (if (= 0 number-lines)
+      t
+    (progn
+      (sit-for 0.02)
+      (scroll-up increment)
+      (smooth-scroll (- number-lines 1) increment))))
 
-    ;; Mouse scrolling
-    (defun smooth-scroll (number-lines increment)
-      (if (= 0 number-lines)
-          t
+(defun set-smooth-scroll ()
+  ;; The scrolling experience is good enough
+  (unless (fboundp 'mac-mwheel-scroll)
+    (if (window-system)
         (progn
-          (sit-for 0.02)
-          (scroll-up increment)
-          (smooth-scroll (- number-lines 1) increment))))
+          (global-set-key (kbd "<wheel-down>")
+                          (lambda () (interactive) (smooth-scroll 3 1)))
+          (global-set-key (kbd "<wheel-up>")
+                          (lambda () (interactive) (smooth-scroll 3 -1)))))
+    (progn
+      (global-set-key [(mouse-5)]
+                      (lambda () (interactive) (smooth-scroll 3 1)))
+      (global-set-key [(mouse-4)]
+                      (lambda () (interactive) (smooth-scroll 3 -1))))))
 
-    (global-set-key [(mouse-5)] (lambda () (interactive) (smooth-scroll 3 1)))
-    (global-set-key [(mouse-4)] (lambda () (interactive) (smooth-scroll 3 -1)))
-    )
-  )
+(defun set-text-mode-mouse-options ()
+  ;; Enable mouse
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
 
-(set-mouse-options)
+  (set-smooth-scroll))
+
+(defun set-window-system-mouse-options ()
+  (set-smooth-scroll))
+
+(if (window-system)
+    (set-window-system-mouse-options)
+  (set-text-mode-mouse-options))
 
 (provide 'mouse-config)
