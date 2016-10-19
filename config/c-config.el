cfdd00eb0c288a6e4145d467ecabfc7bc13b9232
Brad Jensen <bradjensen@apple.com>
2016-10-18 17:31:40 -0700

    Adaptive-wrap never lived up to the hype.

--- config/c-config.el
+++ config/c-config.el
@@ -59,13 +59,6 @@
         (awk-mode . "awk")
         (other . "gnu"))))
 
-(require-package 'adaptive-wrap)
-
-(add-hook 'c-mode-common-hook 'visual-line-mode)
-(add-hook 'c-mode-common-hook 'adaptive-wrap-prefix-mode)
-(add-hook 'c-mode-common-hook (lambda ()
-                                (setq adaptive-wrap-extra-indent 4)))
-
 (require 'flymake-config)
 (add-hook 'c-mode-common-hook 'init-c-flymake)
 
