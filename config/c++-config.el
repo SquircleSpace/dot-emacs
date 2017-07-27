4f83f93a504a25bce56f051cafe372f9839f0a02
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2017-07-27 13:38:11 -0700

    Let's try a different heuristic for recognizing ObjC and C++ files

--- config/c++-config.el
+++ config/c++-config.el
@@ -1,23 +1,5 @@
-(defun current-file-is-c++-p ()
-  (and (string= (file-name-extension buffer-file-name) "h")
-       (or (re-search-forward "[^@]class"
-                              magic-mode-regexp-match-limit
-                              t)
-           (re-search-forward "namespace"
-                              magic-mode-regexp-match-limit
-                              t)
-           (re-search-forward "virtual"
-                              magic-mode-regexp-match-limit
-                              t)
-           (re-search-forward "private:"
-                              magic-mode-regexp-match-limit
-                              t)
-           (re-search-forward "public:"
-                              magic-mode-regexp-match-limit
-                              t))))
-
 (use-package c-config
-  :no-require
-  :magic (current-file-is-c++-p . c++-mode))
+  :no-require t
+  :magic (((rx (sequence line-start "#include <" (+ alpha) ">" line-end)) . c++-mode)))
 
 (provide 'c++-config)
--- config/objc-config.el
+++ config/objc-config.el
@@ -1,17 +1,5 @@
-(defun current-file-is-objc-p ()
-  (and (string= (file-name-extension buffer-file-name) "h")
-       (or (re-search-forward "@\\<interface\\>"
-                              magic-mode-regexp-match-limit
-                              t)
-           (re-search-forward "@\\<protocol\\>"
-                              magic-mode-regexp-match-limit
-                              t)
-           (re-search-forward "@\\<class\\>"
-                              magic-mode-regexp-match-limit
-                              t))))
-
 (use-package c-config
-  :magic ((current-file-is-objc-p . objc-mode)))
+  :magic (((rx (sequence line-start "#import <Foundation/Foundation.h>")) . objc-mode)))
 
 (use-package find-file
   :no-require t
