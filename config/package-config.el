c1b784074080a05a17375acb51ce9b10a5d40101
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2017-07-27 17:19:55 -0700

    Streamline the bootstrapping process
    The previous config couldn't bootstrap without human help

--- config/package-config.el
+++ config/package-config.el
@@ -1,14 +1,14 @@
 ;; Bootstrap package installation
-(add-to-list 'package-selected-packages 'use-package)
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(setf use-package-always-ensure t)
-
-;; Init package
 (add-to-list 'package-archives
              '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(add-to-list 'package-selected-packages 'use-package)
+
+(setf use-package-always-ensure t)
 
 (provide 'package-config)
