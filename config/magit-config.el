4b231c074774681d9a18be17418ce6e0e299cd49
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2017-11-17 10:12:23 -0800

    Update git config
    Default log arguments and disabling vc-git-resolve-conflicts

--- config/magit-config.el
+++ config/magit-config.el
@@ -1,6 +1,8 @@
 (use-package magit
   :bind (("C-c s" . magit-status)
-         ("C-c f" . magit-file-popup)))
+         ("C-c f" . magit-file-popup))
+  :config
+  (setf magit-log-arguments '("--graph" "--color" "--decorate" "-n256")))
 
 ;; Not strictly magit, but useful and gitty
 (use-package git-gutter+
@@ -10,5 +12,10 @@
 
 (use-package git-gutter-fringe+
   :after git-gutter+)
+
+(use-package vc
+  :ensure nil
+  :config
+  (setf vc-git-resolve-conflicts nil))
 
 (provide 'magit-config)
