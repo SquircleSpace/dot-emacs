d252706132f55e306077d4d9a06cd2d959e11b26
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2020-09-28 23:52:20 -0700

    Get off marmalade
    Its been dead for ages

--- config/package-config.el
+++ config/package-config.el
@@ -1,7 +1,5 @@
 ;; Bootstrap package installation
 (add-to-list 'package-archives
-             '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
 (unless (package-installed-p 'use-package)
